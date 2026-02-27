fd0316321e635a22717003ad98ba80b9463f6cfc
youssef hamada <youssef.hamada0101@gmail.com>
2026-02-27 15:10:57 +0200

    first commit

---
+++ task1_dart/bin/task1_dart.dart
@@ -0,0 +1,5 @@
+import 'package:task1_dart/task1_dart.dart' as task1_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${task1_dart.calculate()}!');
+}
---
+++ task1_dart/test/task1_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:task1_dart/task1_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}

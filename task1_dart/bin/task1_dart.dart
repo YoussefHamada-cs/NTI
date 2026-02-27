1d2a93eede5a5044460880908fb955a53bb5be19
youssef hamada <youssef.hamada0101@gmail.com>
2026-02-27 15:15:30 +0200

    finsh task1 dart

--- task1_dart/bin/task1_dart.dart
+++ task1_dart/bin/task1_dart.dart
@@ -1,5 +1,14 @@
-import 'package:task1_dart/task1_dart.dart' as task1_dart;
+import 'package:task1_dart/task1_dart.dart' ;
 
-void main(List<String> arguments) {
-  print('Hello world: ${task1_dart.calculate()}!');
+void main() {
+  print(logIn('student', 'iti123', true));
+  print(calculateFinalPrice(200, true, false));
+  print(examResult(80, 80));
+  print(loans(6000, 25, false));
+  print(deliveryCost(250, 10));
+  print(bonus(yearsOfExperience: 5, rate: 4));
+  print(openDoor(hasAccessCard: true, knowsPassword: true));
+  print(electricityUsage(electricityUsage: 50));
+  print(checkLevel(10, 75));
+  print(driver(true, 100, 50));
 }
--- task1_dart/lib/task1_dart.dart
+++ task1_dart/lib/task1_dart.dart
@@ -1,3 +1,75 @@
-int calculate() {
-  return 6 * 7;
+
+String logIn(String username , String password,bool isAccountActive ){
+  
+ 
+if (username=='student'&&password=='iti123'&&isAccountActive) {
+
+  return "Login successful!";
+}else{
+
+  return "Access Denied!";
+}
+
+}
+num calculateFinalPrice(num productPrice,bool  isPremium,bool hasCoupon){
+ 
+  if (isPremium || hasCoupon) {
+    productPrice *=0.85 ; 
+  }
+  
+  return productPrice;
+ 
+}
+String examResult(int examScore, double attendancePercentage){
+  if (examScore>=50&&attendancePercentage>=75) {
+    return "Passed";
+  } else {
+    return "Failed";
+  }
+
+}
+String loans(int salary,int age,bool hasExistingLoan){
+  if (salary>=5000&&age>=21&&age<=60&&!hasExistingLoan) {
+    return "Loan approved";
+  } else {
+    return "Loan rejected";
+  }
+}
+int deliveryCost(int orderAmount,int distance){
+  if (orderAmount>=300) {
+    return orderAmount;
+  } else {
+    return orderAmount+distance*5;
+  }
+}
+
+String bonus({required int yearsOfExperience, int rate=0}){
+  if (yearsOfExperience>=3&&(rate==4||rate==5)) {
+    return "Bonus Granted";
+  } else {
+    return "No Bonus";
+  }
+}
+String openDoor({required bool hasAccessCard,required bool knowsPassword})=>
+  (hasAccessCard&&knowsPassword)? 'Door opened':'Access Restricted';
+
+String electricityUsage({required int electricityUsage }) {
+  if (electricityUsage < 200) {
+    return "Low Consumption";
+  } else if ( electricityUsage <=500) {
+    return "Medium Consumption";
+  } else {
+    return "High Consumption";
+  }
+}
+
+String checkLevel(int completedLessons,int quizScore)=>
+  (completedLessons>=10&&quizScore>=70)? 'Level Unlocked':'Complete Requirements';
+
+String driver(bool driverAvailability,int userBalance,int rideCost){
+  if (driverAvailability&&userBalance>=rideCost) {
+    return 'Ride Confirmed';
+  } else {
+    return 'Insufficient Conditions';
+  }
 }
